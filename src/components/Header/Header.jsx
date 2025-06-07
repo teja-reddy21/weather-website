0c4aa8b1c92354b6c34dc50261a71e33d499f19b
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-06-07 20:50:02 +0530

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+   <section className='header-section'>
+    <div>
+        <ion-icon name="location-outline"></ion-icon>
+        <span>Kadapa,india</span>
+    </div>
+    <div>
+        <ion-icon name="search-outline"></ion-icon>
+        <input type="text" placeholder="search here"/>
+    </div>
+    <div>
+        <ion-icon name="calendar-outline"></ion-icon>
+        <ion-icon name="notifications-outline"></ion-icon>
+    </div>
+   </section>
+  )
+}
+
+export default Header
