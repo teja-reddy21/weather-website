a8ece7015cc9412491647cdc5de025c3679cce2c
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-06-09 19:21:40 +0530

    dashboard

--- src/components/DashBoard/Dashboard.jsx
+++ src/components/DashBoard/Dashboard.jsx
@@ -63,8 +63,125 @@ export const Dashboard = () => {
             <span>7<sup>o</sup></span>
           </div>
         </div>
+         <div>
+          <div>
+            <img src={SunCloudy} alt=''/>
+            <div>
+              <span>Cloudy</span>
+              <span>Heavy</span>
+            </div>
+          </div>
+          <div>
+            <span>18<sup>o</sup></span>
+          </div>
+        </div>
+         <div>
+          <div>
+            <img src={Rain} alt=''/>
+            <div>
+              <span>Rain</span>
+              <span>Normal</span>
+            </div>
+          </div>
+          <div>
+            <span>2<sup>o</sup></span>
+          </div>
+        </div>
+         <div>
+          <div>
+            <img src={Drops} alt=''/>
+            <div>
+              <span>Humidity</span>
+              <span>Heavy</span>
+            </div>
+          </div>
+          <div>
+            <span>65<sup>o</sup></span>
+          </div>
+        </div>
+         <div>
+          <div>
+            <img src={Ultraviolet} alt=''/>
+            <div>
+              <span>Ultraviolet</span>
+              <span>heavy</span>
+            </div>
+          </div>
+          <div>
+            <span>7<sup>o</sup></span>
+          </div>
+        </div>
+         <div>
+          <div>
+            <img src={PartlySunny} alt=''/>
+            <div>
+              <span>Cloudy</span>
+              <span>Normal</span>
+            </div>
+          </div>
+          <div>
+            <span>26<sup>km/h</sup></span>
+          </div>
+        </div>
       </div>
     </div>
+    </div>
+    <div className='cities'>
+      <h2>Other Cities</h2>
+      <div className='all-cities'>
+        <div>
+          <div>
+            <img src={SunCloudy} alt=''/>
+            <div>
+              <span>Manchester</span>
+              <span>Cloudy, High: 11 Low: 18</span>
+            </div>
+          </div>
+          <div>
+            <span>7<sup>o</sup></span>
+          </div>
+        </div>
+         <div>
+          <div>
+            <img src={Rain} alt=''/>
+            <div>
+              <span>Manchester</span>
+              <span>Cloudy, High: 11 Low: 18</span>
+            </div>
+          </div>
+          <div>
+            <span>19<sup>o</sup></span>
+          </div>
+        </div>
+         <div>
+          <div>
+            <img src={SunCloudy} alt=''/>
+            <div>
+              <span>Manchester</span>
+              <span>Cloudy, High: 11 Low: 18</span>
+            </div>
+          </div>
+          <div>
+            <span>30<sup>o</sup></span>
+          </div>
+        </div>
+         <div>
+          <div>
+            <img src={SunCloudy} alt=''/>
+            <div>
+              <span>Manchester</span>
+              <span>Cloudy, High: 11 Low: 18</span>
+            </div>
+          </div>
+          <div>
+            <span>15<sup>o</sup></span>
+          </div>
+        </div>
+        <button>
+          <span>See More</span>
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+        </button>
+      </div>
     </div>
    </section>
   )
